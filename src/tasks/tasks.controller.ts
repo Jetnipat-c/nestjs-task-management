5a791c567fd3b1c43540c23a7aec547d37a7b4c1
alonecheer <alonecheer@hotmail.com>
2020-03-15 13:51:24 +0700

    @Delete

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -21,5 +21,10 @@ export class TasksController {
     @Post()
     createTasks(@Body() createTasksDto: CreateTaskDto):Task {
         return this.tasksService.createTasks(createTasksDto);
+    }
+
+    @Delete('/:id')
+    deleteTask(@Param('id') id: string){
+        this.tasksService.deleteTask(id);
     }
 }
